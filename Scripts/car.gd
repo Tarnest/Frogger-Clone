02a8c420e9ad5437c8e3d24255645ce4b7474893
Tarnest <tristanconnolly1@gmail.com>
2024-01-24 15:22:45 -0500

    Updated Car Scene

--- Scripts/car.gd
+++ Scripts/car.gd
@@ -1,19 +1,39 @@
-extends RigidBody2D
+extends CharacterBody2D
 
-var speed = 300
-var velocity
-
-@export var direction = "right"
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#
+@export var dir = "right"
+@export var speed = 100
+var direction
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if direction == "right":
-		velocity = Vector2.RIGHT
-	elif direction == "left":
-		velocity = Vector2.LEFT
+	if dir == "right":
+		direction = Vector2.RIGHT
+	elif dir == "left":
+		direction = Vector2.LEFT
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
+	# Add the gravity.
+	# if not is_on_floor():
+	# 	velocity.y += gravity * delta
+
+	# Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction = Input.get_axis("ui_left", "ui_right")
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+	#move_and_slide()
 	
-	pass
+	velocity = direction * speed
+	
+	move_and_slide()
